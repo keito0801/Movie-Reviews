5570c0e4aa68a5ed4a3a116362bad441bc700fb8
Keith Gonzalez <kg0801@yahoo.com>
2021-06-10 00:14:33 -0400

    fixes

--- reviews/models.py
+++ reviews/models.py
@@ -3,16 +3,7 @@ import numpy as np
 
 
 
-# Create your models here.
-class Movie(models.Model):
-    name = models.CharField(max_length=200)
 
-    def average_rating(self):
-        all_rating = map(lambda x: x.rating, self.review_set.all())
-        return np.mean(all_ratings)
-
-        def _unicode_(self):
-            return self.name
 
 class Review(models.Model):
     RATING_CHOICES = (
@@ -22,7 +13,7 @@ class Review(models.Model):
         (4, '4'),
         (5, '5'),
     )
-    #movie = models.CharField(max_length=100)
+    
     title = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
     comment = models.TextField()
